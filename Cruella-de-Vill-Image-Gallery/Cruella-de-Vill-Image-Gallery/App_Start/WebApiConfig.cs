519939311e4fe4e556eea8cbe826d77dc6530ead
snke07 <delqn.dimitrov@gmail.com>
2013-08-15 16:50:02 +0300

    Route added for picture controller.

--- Cruella-de-Vill-Image-Gallery/Cruella-de-Vill-Image-Gallery/App_Start/WebApiConfig.cs
+++ Cruella-de-Vill-Image-Gallery/Cruella-de-Vill-Image-Gallery/App_Start/WebApiConfig.cs
@@ -10,6 +10,12 @@ namespace CruellaDeVillImageGallery
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
+                name: "PicturesApi",
+                routeTemplate: "api/pictures/{action}/{sessionKey}",
+                defaults: new { controller = "pictures" }
+                );
+
+            config.Routes.MapHttpRoute(
                 name: "AlbumsApi",
                 routeTemplate: "api/albums/{albumId}/{action}/{sessionKey}",
                 defaults: new { controller = "albums" }
